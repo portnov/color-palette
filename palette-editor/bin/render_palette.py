e51eec1efa645e44812a97a5ed4c03798418f9dc
Ilya Portnov <portnov@iportnov.ru>
2014-09-11 20:53:48 +0600

    Add render_palette script.

---
+++ palette-editor/bin/render_palette.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import sys
+from os.path import join, basename, dirname, abspath, exists
+import gettext
+
+gettext.install("colors", localedir="share/locale", unicode=True)
+
+bindir = dirname(sys.argv[0])
+rootdir = dirname(bindir)
+sys.path.append(rootdir)
+
+from palette.image import PaletteImage
+from dialogs.open_palette import load_palette
+
+srcfile = sys.argv[1]
+dstfile = sys.argv[2]
+
+palette = load_palette(srcfile)
+w,h = palette.ncols * 48, palette.nrows * 48
+image = PaletteImage(palette).get(w,h)
+image.save(dstfile)
+
