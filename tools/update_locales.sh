719f7dd5a5fc4f6f92d75e6d3207b6acfd79b8ee
Ilya Portnov <portnov@iportnov.ru>
2014-09-15 23:45:30 +0600

    i18n.

--- tools/update_locales.sh
+++ tools/update_locales.sh
@@ -2,7 +2,7 @@
 
 cd palette-editor/
 
-pygettext -v -o share/locale/colors.pot {.,bin,color,dialogs,matching,palette,widgets}/*.py palette/storage/*.py
+pygettext -v -o share/locale/colors.pot {.,bin,color,dialogs,matching,palette,widgets}/*.py widgets/commands/*.py palette/storage/*.py
 cd share/locale
 for D in $(ls -d *)
 do if [ -d $D ]
